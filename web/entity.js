3c18cc9e54753698119f459a8f55ef85165e55fe
Karanstr <139508165+Karanstr@users.noreply.github.com>
2025-09-26 17:34:46 -0400

    New file :party:

---
+++ web/entity.js
@@ -0,0 +1,54 @@
+import Vec2 from "./math.js";
+
+export default class Entity {
+  constructor(pos = new Vec2(), color = "Black") {
+    this.pos = pos;
+    this.priority = 0;
+    this.color = color;
+    this.outline = false;
+    this.points = []; // Points = [Vec2]
+  }
+
+  update_material(material) {
+    switch (material) {
+      case 0: // Player
+        this.color = "white";
+        this.outline = true;
+        this.priority = 3;
+        break;
+      case 1: // Wall
+        this.color = "black";
+        this.priority = 1;
+        break;
+      case 2: // Death
+        this.color = "red";
+        this.priority = 2;
+        break;
+      case 3: // WinOn
+        this.color = "green";
+        this.priority = 1;
+        break;
+      case 4: // WinOff
+        this.color = "lime";
+        this.priority = 1;
+        break;
+    }
+  }
+  
+  render(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.moveTo(this.points[0].x + this.pos.x, this.points[0].y + this.pos.y);
+    for (let i = 1; i < this.points.length; i++) {
+      let translated = this.points[i].add(this.pos);
+      ctx.lineTo(translated.x, translated.y);
+    }
+    ctx.closePath();
+    ctx.fill();
+    if (this.outline) {
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 1;
+      ctx.stroke();
+    }
+  }
+}
